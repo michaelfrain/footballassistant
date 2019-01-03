aa52a84e7b0a7a7258973fbc087fb1a8973a43ee
Michael Frain <michael.frain@hudl.com>
2019-01-03 11:38:22 -0500

    Set up first controllers

---
+++ footballassistant WatchKit Extension/GameClockController.swift
@@ -0,0 +1,13 @@
+//
+//  GameClockController.swift
+//  footballassistant WatchKit Extension
+//
+//  Created by Michael Frain on 1/3/19.
+//  Copyright © 2019 mfrain. All rights reserved.
+//
+
+import UIKit
+
+class GameClockController: WKInterfaceController {
+
+}
---
+++ footballassistant WatchKit Extension/TimerController.swift
@@ -0,0 +1,13 @@
+//
+//  TimerController.swift
+//  footballassistant WatchKit Extension
+//
+//  Created by Michael Frain on 1/3/19.
+//  Copyright © 2019 mfrain. All rights reserved.
+//
+
+import UIKit
+
+class TimerController: WKInterfaceController {
+
+}
